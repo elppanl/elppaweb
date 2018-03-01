e182592d421aaab73bf5c0eeea7ce468e0792b98
elppanl <elppanl@126.com>
2018-03-01 12:29:55 +0800

    ep1

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <div id="head">这里是主导航</div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/elpp1.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>这里是首页的内容</p>
+</template>
+
+<script>
+    export default{
+
+    }
+</script>
+
+<style scoped>
+
+</style>
+
