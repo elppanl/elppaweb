49a800adc9060bb50057021c0486c09cc0e1bcd1
elppanl <elppanl@126.com>
2018-03-11 20:32:49 +0800

    ax2

--- src/components/pages/elpp2p1.vue
+++ src/components/pages/elpp2p1.vue
@@ -7,6 +7,54 @@
         <div id="artbox" :class="{'p2artm':p2am, 'p2artpc':p2apc}">
             <div id="artleft" v-if="p2aleft">
                 <h2>文章目录</h2>
+
+                <el-row class="tac">
+                <el-col :span="12" id="cai1">
+                    <el-menu
+                    default-active="2"
+                    class="el-menu-vertical-demo"
+                    @open="handleOpen"
+                    @close="handleClose">
+                    <el-submenu index="1">
+                        <template slot="title">
+                        <i class="el-icon-document"></i>
+                        <span>导航一</span>
+                        </template>
+                        <!-- <el-menu-item-group> -->
+                        <!-- <template slot="title">分组一</template> -->
+                        <el-menu-item index="1-1">选项1</el-menu-item>
+                        <el-menu-item index="1-2">选项2</el-menu-item>
+                        <!-- </el-menu-item-group> -->
+                        <!-- <el-menu-item-group title="分组2"> -->
+                        <el-menu-item index="1-3">选项3</el-menu-item>
+                        <!-- </el-menu-item-group> -->
+                        <el-submenu index="1-4">
+                        <template slot="title">选项4</template>
+                        <el-menu-item index="1-4-1">选项1</el-menu-item>
+                        </el-submenu>
+                    </el-submenu>
+                    <el-submenu index="2">
+                        <template slot="title">
+                        <i class="el-icon-document"></i>
+                        <span>导航二</span>
+                        </template>
+                        <!-- <el-menu-item-group> -->
+                        <!-- <template slot="title">分组一</template> -->
+                        <el-menu-item index="2-1">选项1</el-menu-item>
+                        <el-menu-item index="2-2">选项2</el-menu-item>
+                        <!-- </el-menu-item-group> -->
+                        <!-- <el-menu-item-group title="分组2"> -->
+                        <el-menu-item index="2-3">选项3</el-menu-item>
+                        <!-- </el-menu-item-group> -->
+                        <el-submenu index="2-4">
+                        <template slot="title">选项4</template>
+                        <el-menu-item index="2-4-1">选项1</el-menu-item>
+                        </el-submenu>
+                    </el-submenu>
+                    </el-menu>
+                </el-col>
+                </el-row>
+
                 <h1 @click="inx1">aaa</h1>
                 <h1 @click="inx2">bbb</h1>
                 <h1 @click="inx3">ccc</h1>
@@ -192,6 +240,10 @@
     }
     .clear{
         clear: both;
+    }
+    #cai1{
+        width: 100%;
+        text-align: left;
     }
 </style>
 
