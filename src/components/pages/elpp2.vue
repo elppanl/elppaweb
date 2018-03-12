176bb94dfb612c3b358c9da4fa24b31b7b38f389
elppanl <elppanl@126.com>
2018-03-12 13:19:46 +0800

    aaa

--- src/components/pages/elpp2.vue
+++ src/components/pages/elpp2.vue
@@ -7,12 +7,138 @@
             <img src="../../assets/ptu1.jpg" alt="ptu1">
         </div>
         <div id="p2p1b2" :class="{'p2bm':p2m, 'p2bpc':p2pc}">
-            <router-link to="/elpp2p1" class="p2block" :class="{'p2blockm':p2m, 'p2blockpc':p2pc}"></router-link>
-            <router-link to="/elpp2p1" class="p2block" :class="{'p2blockm':p2m, 'p2blockpc':p2pc}"></router-link>
-            <router-link to="/elpp2p1" class="p2block" :class="{'p2blockm':p2m, 'p2blockpc':p2pc}"></router-link>
-            <router-link to="/elpp2p1" class="p2block" :class="{'p2blockm':p2m, 'p2blockpc':p2pc}"></router-link>
-            <router-link to="/elpp2p1" class="p2block" :class="{'p2blockm':p2m, 'p2blockpc':p2pc}"></router-link>
-            <router-link to="/elpp2p1" class="p2block" :class="{'p2blockm':p2m, 'p2blockpc':p2pc}"></router-link>
+            <!-- 文章块开始 -->
+            <router-link to="/artemplate1" class="p2block" :class="{'p2blockm':p2m, 'p2blockpc':p2pc}">
+                <div class="p2b2boxs" :class="{'p2b2boxsm':p2m, 'p2b2boxspc':p2pc}">
+                    <div class="p2b2imgbox" :class="{'p2b2imgboxm':p2m, 'p2b2imgboxpc':p2pc}">
+                        <img src="../../assets/elplogo1.png">
+                    </div>
+                    <div class="p2b2textbox" :class="{'p2b2textboxm':p2m, 'p2b2textboxpc':p2pc}">
+                        <div class="p2b2text1" :class="{'p2b2text1m':p2m, 'p2b2text1pc':p2pc}">
+                            <p>这里是文章题目文章待更新敬请期待</p>
+                        </div>
+                        <div class="p2b2text2" :class="{'p2b2text2m':p2m, 'p2b2text2pc':p2pc}">
+                            <p>2088年12月12日</p>
+                        </div>
+                        <div class="p2b2text3" v-if="show1">
+                            <p>这里是文章的说明，简要介绍文章的主题内容。待更新，敬请期待。</p>
+                        </div>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            </router-link>
+            <!-- 文章块结束 -->
+
+            <!-- 文章块开始 -->
+            <router-link to="/artemplate1" class="p2block" :class="{'p2blockm':p2m, 'p2blockpc':p2pc}">
+                <div class="p2b2boxs" :class="{'p2b2boxsm':p2m, 'p2b2boxspc':p2pc}">
+                    <div class="p2b2imgbox" :class="{'p2b2imgboxm':p2m, 'p2b2imgboxpc':p2pc}">
+                        <img src="../../assets/elplogo1.png">
+                    </div>
+                    <div class="p2b2textbox" :class="{'p2b2textboxm':p2m, 'p2b2textboxpc':p2pc}">
+                        <div class="p2b2text1" :class="{'p2b2text1m':p2m, 'p2b2text1pc':p2pc}">
+                            <p>这里是文章题目文章待更新敬请期待</p>
+                        </div>
+                        <div class="p2b2text2" :class="{'p2b2text2m':p2m, 'p2b2text2pc':p2pc}">
+                            <p>2088年12月12日</p>
+                        </div>
+                        <div class="p2b2text3" v-if="show1">
+                            <p>这里是文章的说明，简要介绍文章的主题内容。待更新，敬请期待。</p>
+                        </div>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            </router-link>
+            <!-- 文章块结束 -->
+
+            <!-- 文章块开始 -->
+            <router-link to="/artemplate1" class="p2block" :class="{'p2blockm':p2m, 'p2blockpc':p2pc}">
+                <div class="p2b2boxs" :class="{'p2b2boxsm':p2m, 'p2b2boxspc':p2pc}">
+                    <div class="p2b2imgbox" :class="{'p2b2imgboxm':p2m, 'p2b2imgboxpc':p2pc}">
+                        <img src="../../assets/elplogo1.png">
+                    </div>
+                    <div class="p2b2textbox" :class="{'p2b2textboxm':p2m, 'p2b2textboxpc':p2pc}">
+                        <div class="p2b2text1" :class="{'p2b2text1m':p2m, 'p2b2text1pc':p2pc}">
+                            <p>这里是文章题目文章待更新敬请期待</p>
+                        </div>
+                        <div class="p2b2text2" :class="{'p2b2text2m':p2m, 'p2b2text2pc':p2pc}">
+                            <p>2088年12月12日</p>
+                        </div>
+                        <div class="p2b2text3" v-if="show1">
+                            <p>这里是文章的说明，简要介绍文章的主题内容。待更新，敬请期待。</p>
+                        </div>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            </router-link>
+            <!-- 文章块结束 -->
+
+            <!-- 文章块开始 -->
+            <router-link to="/artemplate1" class="p2block" :class="{'p2blockm':p2m, 'p2blockpc':p2pc}">
+                <div class="p2b2boxs" :class="{'p2b2boxsm':p2m, 'p2b2boxspc':p2pc}">
+                    <div class="p2b2imgbox" :class="{'p2b2imgboxm':p2m, 'p2b2imgboxpc':p2pc}">
+                        <img src="../../assets/elplogo1.png">
+                    </div>
+                    <div class="p2b2textbox" :class="{'p2b2textboxm':p2m, 'p2b2textboxpc':p2pc}">
+                        <div class="p2b2text1" :class="{'p2b2text1m':p2m, 'p2b2text1pc':p2pc}">
+                            <p>这里是文章题目文章待更新敬请期待</p>
+                        </div>
+                        <div class="p2b2text2" :class="{'p2b2text2m':p2m, 'p2b2text2pc':p2pc}">
+                            <p>2088年12月12日</p>
+                        </div>
+                        <div class="p2b2text3" v-if="show1">
+                            <p>这里是文章的说明，简要介绍文章的主题内容。待更新，敬请期待。</p>
+                        </div>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            </router-link>
+            <!-- 文章块结束 -->
+
+            <!-- 文章块开始 -->
+            <router-link to="/artemplate1" class="p2block" :class="{'p2blockm':p2m, 'p2blockpc':p2pc}">
+                <div class="p2b2boxs" :class="{'p2b2boxsm':p2m, 'p2b2boxspc':p2pc}">
+                    <div class="p2b2imgbox" :class="{'p2b2imgboxm':p2m, 'p2b2imgboxpc':p2pc}">
+                        <img src="../../assets/elplogo1.png">
+                    </div>
+                    <div class="p2b2textbox" :class="{'p2b2textboxm':p2m, 'p2b2textboxpc':p2pc}">
+                        <div class="p2b2text1" :class="{'p2b2text1m':p2m, 'p2b2text1pc':p2pc}">
+                            <p>这里是文章题目文章待更新敬请期待</p>
+                        </div>
+                        <div class="p2b2text2" :class="{'p2b2text2m':p2m, 'p2b2text2pc':p2pc}">
+                            <p>2088年12月12日</p>
+                        </div>
+                        <div class="p2b2text3" v-if="show1">
+                            <p>这里是文章的说明，简要介绍文章的主题内容。待更新，敬请期待。</p>
+                        </div>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            </router-link>
+            <!-- 文章块结束 -->
+
+            <!-- 文章块开始 -->
+            <router-link to="/artemplate1" class="p2block" :class="{'p2blockm':p2m, 'p2blockpc':p2pc}">
+                <div class="p2b2boxs" :class="{'p2b2boxsm':p2m, 'p2b2boxspc':p2pc}">
+                    <div class="p2b2imgbox" :class="{'p2b2imgboxm':p2m, 'p2b2imgboxpc':p2pc}">
+                        <img src="../../assets/elplogo1.png">
+                    </div>
+                    <div class="p2b2textbox" :class="{'p2b2textboxm':p2m, 'p2b2textboxpc':p2pc}">
+                        <div class="p2b2text1" :class="{'p2b2text1m':p2m, 'p2b2text1pc':p2pc}">
+                            <p>这里是文章题目文章待更新敬请期待</p>
+                        </div>
+                        <div class="p2b2text2" :class="{'p2b2text2m':p2m, 'p2b2text2pc':p2pc}">
+                            <p>2088年12月12日</p>
+                        </div>
+                        <div class="p2b2text3" v-if="show1">
+                            <p>这里是文章的说明，简要介绍文章的主题内容。待更新，敬请期待。</p>
+                        </div>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            </router-link>
+            <!-- 文章块结束 -->
+
         </div>
     </div>
 </template>
@@ -22,7 +148,8 @@
         data:function(){
             return{
                 p2m:'',
-                p2pc:''
+                p2pc:'',
+                show1:''
             }
         },
         methods:{
@@ -30,9 +157,11 @@
                 if(window.innerWidth>768){
                     this.p2pc=true;
                     this.p2m=false;
+                    this.show1=true;
                 }else{
                     this.p2pc=false;
                     this.p2m=true;
+                    this.show1=false;
                 }
             },
             sf(){
@@ -40,9 +169,11 @@
                 if(window.innerWidth>768){
                     this.p2pc=true;
                     this.p2m=false;
+                    this.show1=true;
                 }else{
                     this.p2pc=false;
                     this.p2m=true;
+                    this.show1=false;
                 }
             }
         },
@@ -67,13 +198,108 @@
     .p2bm{
         width: 100%;
     }
+    .p2b2boxsm{
+        width: 290px;
+        height: 130px;
+        /* border: 1px solid #e9e9e9; */
+        margin: 25px auto;
+    }
+    .p2b2boxspc{
+        width: 420px;
+        height: 150px;
+        /* border: 1px solid #e9e9e9; */
+        margin: 15px auto;
+    }
+    .p2b2imgboxpc{
+        width: 150px;
+        height: 150px;
+        border: 1px solid #e9e9e9;
+        float: left;
+    }
+    .p2b2imgboxm{
+        width: 130px;
+        height: 130px;
+        border: 1px solid #e9e9e9;
+        float: left;
+    }
+    .p2block img{
+        width: 100%;
+        opacity: .5;
+    }
+    .p2b2textboxpc{
+        width: 255px;
+        height: 150px;
+        /* border: 1px solid #e9e9e9; */
+        float: right;
+    }
+    .p2b2textboxm{
+        width: 155px;
+        height: 130px;
+        /* border: 1px solid #e9e9e9; */
+        float: right;
+    }
+
+    .p2b2text1pc{
+        width: 100%;
+        height: 40%;
+        /* border: 1px solid #e9e9e9; */
+    }
+    .p2b2text2pc{
+        width: 100%;
+        height: 20%;
+        /* border: 1px solid #e9e9e9; */
+    }
+    .p2b2text3{
+        width: 100%;
+        height: 40%;
+        /* border: 1px solid #e9e9e9; */
+        border-top: 1px solid #e9e9e9;
+    }
+    .p2b2text1m{
+        width: 100%;
+        height: 80%;
+        /* border: 1px solid #e9e9e9; */
+    }
+    .p2b2text2m{
+        width: 100%;
+        height: 20%;
+        /* border: 1px solid #e9e9e9; */
+    }
+    .p2b2textbox p{
+        margin-top: 0;
+        margin-bottom: 0;
+    }
+    .p2b2text1 p{
+        font-size: 24px;
+        color: #663333;
+        text-align: center;
+        margin-top: 8px;
+    }
+    .p2b2text2 p{
+        color: #663333;
+        font-size: 12px;
+        text-align: center;
+    }
+    .p2b2text2m p{
+        line-height: 24px;
+    }
+    .p2b2text2pc p{
+        line-height: 30px;
+    }
+    .p2b2text3 p{
+        color: #663333;
+        font-size: 15px;
+        line-height: 25px;
+    }
+
     .p2blockpc{
-        width: 485px;
+        width: 482px;
         height: 180px;
         border: 1px solid #e9e9e9;
         border-radius: 5px;
-        display:inline-block;
+        float: left;
         margin-top: 10px;
+        margin-right: 5px;
     }
     .p2blockpc:hover{
         background-color: #e9e9e9;
@@ -90,6 +316,9 @@
     }
     #p2p1b1>img{
         width: 100%;
+    }
+    .clear{
+        clear: both;
     }
 </style>
 
